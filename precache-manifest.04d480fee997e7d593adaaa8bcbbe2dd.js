0980d4b8858986c673612ecf3e80be3849a835fe
MaxV(Vinh) <44563184+MaxV-LTTVinh@users.noreply.github.com>
2020-07-27 16:36:16 +0700

    publish v1.1

---
+++ precache-manifest.04d480fee997e7d593adaaa8bcbbe2dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3eb4d115818b07227e2f55169ddcce07",
+    "url": "/index.html"
+  },
+  {
+    "revision": "20cc55733384b3e85a31",
+    "url": "/static/css/main.8bb9b25a.chunk.css"
+  },
+  {
+    "revision": "0afd4dd5693f105eb710",
+    "url": "/static/js/2.245eabea.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/static/js/2.245eabea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20cc55733384b3e85a31",
+    "url": "/static/js/main.eea241e9.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
